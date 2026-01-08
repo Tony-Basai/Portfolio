439f1622486ed9835d44c4883902f033039740b3
tonibasai29@gmail.com <tonibasai29@gmail.com>
2026-01-09 00:55:37 +0300

    debug + update + adaptive

--- portfolio.js
+++ portfolio.js
@@ -1,72 +1,82 @@
-const button = document.querySelector(".about-me__button");
-const modal = document.querySelector(".modal");
+const itemButton = document.querySelectorAll(".button__plus");
 
-button.addEventListener("click", () => {
-    modal.classList.remove("modal--hidden");
-    document.body.classList.add('no-scroll');
-});
-
-const buttonClose = document.querySelector(".modal__buttom-close");
-
-buttonClose.addEventListener("click", () => {
-    clearModal()
-});
-
-const buttonDownload = document.querySelector(".modal__buttom-download");
-
-buttonDownload.addEventListener("click", () => {
-    clearModal()
-});
-
-function clearModal() {
-        modal.classList.add("modal--hidden");
-        document.body.classList.remove('no-scroll');
-}
-
-buttonDownload.addEventListener ('click', (e) => modalSubmit(e));
-
-function modalSubmit (e) {
-    e.preventDefault();  
-}
-
-buttonClose.addEventListener ('click', (e) => modalSubmit(e));
-
-function modalSubmit (e) {
-    e.preventDefault();  
-}
-
-const itemDate = document.querySelectorAll(".item-accordion__items");
-
-itemDate.forEach((item) =>{
+itemButton.forEach((item) =>{
     item.addEventListener("click", listHandler);
 });
 
 function listHandler (event) {
     event.preventDefault();
-    let returnDate = event.target.closest(".accordion-items__mark");
-    let returnContent = returnDate.nextElementSibling;
+
+    let button = event.currentTarget;
+    let returnButton = event.target.closest(".accordion-items__button");
+    let returnContent = returnButton.nextElementSibling;
     
-    returnDate.classList.toggle("active");
-    if (returnDate.classList.contains("active")) {
+    returnButton.classList.toggle("active");
+
+    if (returnButton.classList.contains("active")) {
         returnContent.style.maxHeight = returnContent.scrollHeight + "rem";
+        button.textContent = "−";
+        returnButton.style.background = "var(--too-light-yellow)";
     } else {
         returnContent.style.maxHeight = 0;
+        button.textContent = "+";
+        returnButton.style.background = "var(--yellow)";
     }
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
-      loop: true,
-      slidesPerView: 2,
-      spaceBetween: 30,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+
+  const button = document.querySelector(".about-me__button");
+  const modal = document.querySelector(".modal");
+  const buttonClose = document.querySelector(".modal__buttom-close");
+  const buttonDownload = document.querySelectorAll(".modal__buttom-download");
+
+  button.addEventListener("click", () => {
+      modal.classList.remove("modal--hidden");
+      document.body.classList.add('no-scroll');
+  });
+
+  buttonClose.addEventListener("click", clearModal);
+
+  buttonDownload.forEach(button => {
+      button.addEventListener("click", clearModal)
+  });
+
+  function clearModal() {
+          modal.classList.add("modal--hidden");
+          document.body.classList.remove('no-scroll');
+  }
+
+  const swiper = new Swiper('.swiper', {
+    autoplay:{
+      delay: 6000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },  
+    keyboard:{
+      enabled: true,
+      onlyInViewport: true,
+    },
+    loop: true,
+    slidesPerView: 2,
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 16,
+    breakpoints: {
+      481: {
+        slidesPerView: 2,
+        centeredSlides: false,
+        spaceBetween: 16,
       },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-      },
-    });
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  });
 });
